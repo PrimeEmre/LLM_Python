f368e3334ac9e867740b79d9969b623de2b238e4
PrimeEmre <ynsemrgzl@gmail.com>
2026-02-28 14:54:54 -0500

    added the text file

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello World")
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# reading the file
+with open ("input.txt", "r",encoding="utf-8") as f:
+    text = f.read()
+    print(f"Total characters in dataset: {len(text)}")
+
+    # getting the characters
+    chars = sorted(list(set(text)))
+    vocab_size = len(chars)
+    print(f"Unique characters in dataset: {vocab_size}")
+
+    # converting the
